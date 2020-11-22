b6c02165ecd0a300abb923cdb138bec40579421d
anilson.monteiro <anilson.monteiro@nosi.cv>
2020-11-22 11:07:29 -0100

    update

--- src/Home.js
+++ src/Home.js
@@ -31,21 +31,21 @@ function Home(){
             <div className='home__section'>
             <Card
                 src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
-                title="3 Bedroom Flat in Bournemouth"
+                title="3 Bedroom Flat in Praia"
                 description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                price="£130/night"
+                price="EUR 130/night"
             />
             <Card
                 src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-                title="Penthouse in London"
+                title="House in Tarrafal"
                 description="Enjoy the amazing sights of London with this stunning penthouse"
-                price="£350/night"
+                price="EUR 350/night"
             />
             <Card
                 src="https://media.nomadicmatt.com/2018/apartment.jpg"
                 title="1 Bedroom apartment"
                 description="Superhost with great amenities and a fabolous shopping complex nearby"
-                price="£70/night"
+                price="EUR 70/night"
             />
             </div>
         </div>
